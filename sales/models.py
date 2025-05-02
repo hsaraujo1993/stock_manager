c922cbe071e736ffe7b5fefd494cc4b1517c873d
hsaraujo1993 <hsaraujo1993@gmail.com>
2025-05-02 13:36:33 -0300

    feat: adding price update after deleting item

--- sales/models.py
+++ sales/models.py
@@ -1,25 +1,17 @@
 from django.db import models
 from django.apps import AppConfig
+from decimal import Decimal
 
 
 # Create your models here.
 
 class Sale(models.Model):
     date = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
-    total_value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Valor Total', default=0)
+    total_value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Valor Total', default=Decimal('0.00'))
 
     class Meta:
         verbose_name = 'Venda'
         verbose_name_plural = 'Vendas'
-
-    def total(self):
-        # Calcula o valor total da venda somando os subtotais dos itens
-        return sum(item.subtotal() for item in self.items.all()) if self.pk else 0
-
-    def save(self, *args, **kwargs):
-        # Removemos a atualização automática do total_value aqui
-        # para evitar problemas com objetos novos que não têm itens ainda
-        super().save(*args, **kwargs)
 
     def __str__(self):
         if self.id is None or self.date is None:
@@ -28,7 +20,13 @@ class Sale(models.Model):
 
     @property
     def total(self):
-        return sum(item.subtotal() for item in self.items.all())
+        # Calcula o valor total da venda somando os subtotais dos itens
+        return sum(item.subtotal() for item in self.items.all()) if self.pk else Decimal('0.00')
+
+    def save(self, *args, **kwargs):
+        # Atualiza o campo total_value automaticamente ao salvar a venda
+        self.total_value = self.total  # Atribui o valor calculado de `total` ao campo `total_value`
+        super().save(*args, **kwargs)
 
 
 class SalesConfig(AppConfig):
--- sales_items/signals.py
+++ sales_items/signals.py
@@ -1,6 +1,5 @@
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_save, post_delete
 from django.dispatch import receiver
-
 from sales_items.models import SalesItem
 
 
@@ -12,3 +11,14 @@ def return_stock_on_delete(sender, instance, **kwargs):
     if product.stock:
         product.stock.quantity += instance.quantity
         product.stock.save()
+
+
+@receiver(post_save, sender=SalesItem)
+@receiver(post_delete, sender=SalesItem)
+def update_sale_total(sender, instance, **kwargs):
+    """
+    Atualiza o total da venda sempre que um SalesItem for alterado ou deletado.
+    """
+    sale = instance.sale
+    sale.total_value = sum(item.subtotal() for item in sale.items.all())  # Recalcula o total
+    sale.save()  # Salva a venda com o novo total
